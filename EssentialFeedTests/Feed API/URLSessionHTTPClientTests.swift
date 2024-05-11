8d26a7360794fc0f1fed44a373dfe199bb3f53b8
Batyr Ovezdurdyyev <batyr.ovezdurdyyev@gmail.com>
2024-05-11 10:49:30 -0500

    Delivers unexpected values error when request completes with data, response and error nil

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -14,11 +14,14 @@ class URLSessionHTTPClient {
     init(session: URLSession = .shared) {
         self.session = session
     }
+    struct UnexpectedValuesRepresentation: Error { }
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
         session.dataTask(with: url) { _, _, error in
             if let error = error {
                 completion(.failure(error))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
             }
         }.resume()
     }
@@ -57,7 +60,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         let exp = expectation(description: "Wait for completion")
         
         makeSUT().get(from: anyURL()) { result in
-            
             switch result {
             case .failure(let receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -65,6 +67,26 @@ class URLSessionHTTPClientTests: XCTestCase {
                 // or XCTAssertNotNil(receivedError)
             default:
                 XCTFail("Expected failure with error \(error), got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_getFromURL_failsOnAllNilValues() {
+        // setup
+        URLProtocolStub.stub(data: nil, response: nil, error: nil)
+                
+        let exp = expectation(description: "Wait for completion")
+        
+        makeSUT().get(from: anyURL()) { result in
+            switch result {
+            case .failure:
+                break
+            default:
+                XCTFail("Expected failure, got \(result) instead")
             }
             
             exp.fulfill()
